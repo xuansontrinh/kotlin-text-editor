da8be49af35ad4602574c6a3097ce07f07273a8d
Xuan-Son Trinh <xuanson.trinh@outlook.com>
2020-12-31 18:46:49 +0100

    test line number feature

--- editor/LineNumbers.py
+++ editor/LineNumbers.py
@@ -4,14 +4,14 @@ class LineNumbers(tk.Text):
     def __init__(self, master, text_widget, **kwargs):
         super().__init__(master, **kwargs)
         self.text_widget = text_widget
-        self.text_widget.bind('<KeyPress>', self.on_key_press)
+        self.text_widget.bind('<<ViewScroll>>', self.onKeyPress)
         self.tag_configure("right", justify=tk.RIGHT)
 
         self.insert(1.0, '1')
         self.tag_add("right", 1.0, "end")
         self.configure(state='disabled', width=2)
 
-    def on_key_press(self, event=None):
+    def onKeyPress(self, event=None):
         final_index = str(self.text_widget.index(tk.END))
         num_of_lines = final_index.split('.')[0]
         line_numbers_string = "\n".join(str(no + 1) for no in range(int(num_of_lines)))
@@ -22,3 +22,25 @@ class LineNumbers(tk.Text):
         self.insert(1.0, line_numbers_string)
         self.tag_add("right", 1.0, "end")
         self.configure(state='disabled')
+
+class TextLineNumbers(tk.Canvas):
+    def __init__(self, *args, **kwargs):
+        tk.Canvas.__init__(self, *args, **kwargs)
+        self.textwidget = None
+
+    def attach(self, text_widget):
+        self.textwidget = text_widget
+
+    def redraw(self, *args, **kwargs):
+        '''redraw line numbers'''
+        self.delete("all")
+
+        i = self.textwidget.index("@0,0")
+        while True :
+            dline= self.textwidget.dlineinfo(i)
+            if dline is None: break
+            y = dline[1]
+            linenum = str(i).split(".")[0]
+            self.config(width=len(linenum) + 2)
+            self.create_text(2,y,anchor=tk.NW, text=linenum, font=kwargs['font'])
+            i = self.textwidget.index("%s+1line" % i)
