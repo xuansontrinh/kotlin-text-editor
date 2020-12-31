0eb2b34095f66636b9575b4fec34cc4b13dba231
Xuan-Son Trinh <xuanson.trinh@outlook.com>
2020-12-31 18:44:48 +0100

    add misc files

--- editor/Common/Utils.py
+++ editor/Common/Utils.py
@@ -1,10 +1,24 @@
 import os
+import tkinter as tk
 import yaml
+
+from PIL import Image, ImageTk
 
 os.chdir(os.path.dirname(os.path.realpath(__file__)))
 
 LANGUAGES_PATH = os.path.join('languages')
 THEMES_PATH = os.path.join('themes')
+ICONS_PATH = os.path.join('resources')
+
+FONT_FAMILY = 'Consolas'
+FONT_SIZE_MAP = {
+    'Small': 15,
+    'Medium': 18,
+    'Large': 20
+}
+FONT_SIZE_DEFAULT = 'Small'
+
+FILE_NAME = "tmp"
 
 def parse_file(path=None):
     with open(path, 'r') as stream:
@@ -20,10 +34,10 @@ def parse_language_file(language='kotlin'):
 def parse_theme_file(theme='dracula'):
     return parse_file(path=os.path.join(THEMES_PATH, '{}.yml'.format(theme)))
 
-FONT_FAMILY = 'Consolas'
-FONT_SIZE_MAP = {
-    'Small': 15,
-    'Medium': 18,
-    'Large': 20
-}
-FONT_SIZE_DEFAULT = 'Small'
+def get_icon(name):
+    image = tk.PhotoImage(file=os.path.join(ICONS_PATH, '{}.png'.format(name)))
+    # image = image.subsample(2) 
+    
+    # image = Image.open(os.path.join(ICONS_PATH, '{}.png'.format(name)))
+    # image = image.resize((5, 5), Image.ANTIALIAS)
+    return image
